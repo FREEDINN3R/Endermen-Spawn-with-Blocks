98f5a953f5561066485cfb5e3da5435a5257e2c2
Nerjal Nosk <nosk.nerjal@gmx.com>
2023-09-05 01:56:45 +0200

    Replaced fixed block options with dynamic tags

--- src/main/java/net/freedinner/endermen_spawn_with_blocks/mixin/EntityTypeMixin.java
+++ src/main/java/net/freedinner/endermen_spawn_with_blocks/mixin/EntityTypeMixin.java
@@ -1,13 +1,13 @@
 package net.freedinner.endermen_spawn_with_blocks.mixin;
 
-import net.freedinner.endermen_spawn_with_blocks.util.AbstractBlockProvider;
-import net.freedinner.endermen_spawn_with_blocks.util.EndBlockProvider;
-import net.freedinner.endermen_spawn_with_blocks.util.NetherBlockProvider;
-import net.freedinner.endermen_spawn_with_blocks.util.OverworldBlockProvider;
+import net.freedinner.endermen_spawn_with_blocks.util.*;
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,23 +16,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EntityType.class)
 public abstract class EntityTypeMixin<T extends Entity> {
     @Inject(method = "create(Lnet/minecraft/world/World;)Lnet/minecraft/entity/Entity;", at = @At("RETURN"))
-    private void onCreate(World world, CallbackInfoReturnable<T> cir) {
-        if (cir.getReturnValue() instanceof EndermanEntity enderman) {
-            if (world.isClient) {
+    private void onCreate(World w, @NotNull CallbackInfoReturnable<T> cir) {
+        if (cir.getReturnValue() instanceof EndermanEntity enderman && w instanceof ServerWorld world) {
+
+            Block b = DynamicBlockProvider.getInstance().getBlock(world);
+
+            if (b == null) {
                 return;
             }
 
-            AbstractBlockProvider blockProvider = switch (world.getRegistryKey().getValue().toString()) {
-                case "minecraft:overworld" -> new OverworldBlockProvider();
-                case "minecraft:the_nether" -> new NetherBlockProvider();
-                default -> new EndBlockProvider();
-            };
-
-            if (!blockProvider.shouldHoldBlock()) {
-                return;
-            }
-
-            enderman.setCarriedBlock(blockProvider.getBlock().getDefaultState());
+            enderman.setCarriedBlock(b.getDefaultState());
         }
     }
 }
