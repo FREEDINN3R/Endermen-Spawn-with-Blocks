1c0204579f28706d2a29141479cf03121660f8de
Nerjal Nosk <nosk.nerjal@gmx.com>
2023-09-05 14:30:52 +0200

    Config improvement and removed pointless entrypoint (pun not intended)

--- src/main/java/net/freedinner/endermen_spawn_with_blocks/EndermenSpawnWithBlocks.java
+++ src/main/java/net/freedinner/endermen_spawn_with_blocks/EndermenSpawnWithBlocks.java
@@ -1,21 +1,15 @@
 package net.freedinner.endermen_spawn_with_blocks;
 
-import net.fabricmc.api.ModInitializer;
-import net.freedinner.endermen_spawn_with_blocks.config.ModConfigs;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EndermenSpawnWithBlocks implements ModInitializer {
+public class EndermenSpawnWithBlocks {
 	public static final String MOD_ID = "endermen_spawn_with_blocks";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		ModConfigs.registerConfigs();
-	}
 
 	@Contract("_ -> new")
 	public static @NotNull Identifier id(@NotNull String path) {
--- src/main/java/net/freedinner/endermen_spawn_with_blocks/config/ModConfigs.java
+++ src/main/java/net/freedinner/endermen_spawn_with_blocks/config/ModConfigs.java
@@ -2,38 +2,35 @@ package net.freedinner.endermen_spawn_with_blocks.config;
 
 import net.freedinner.endermen_spawn_with_blocks.EndermenSpawnWithBlocks;
 import net.minecraft.util.math.MathHelper;
-
-import java.util.ArrayList;
+import org.jetbrains.annotations.NotNull;
 
 public class ModConfigs {
-    public static SimpleConfig CONFIG;
+    public static final SimpleConfig CONFIG;
 
-    public static double BLOCK_SPAWN_CHANCE;
+    public static final double BLOCK_SPAWN_CHANCE;
     private static final String BLOCK_SPAWN_CHANCE_KEY = "block_spawn_chance";
     private static final double BLOCK_SPAWN_CHANCE_DEFAULT = 0.3 ;
 
 
-    public static void registerConfigs() {
+    static {
         EndermenSpawnWithBlocks.LOGGER.info("Registering configs");
 
         ModConfigProvider configProvider = new ModConfigProvider();
         initializeConfig(configProvider);
 
         CONFIG = SimpleConfig.of(EndermenSpawnWithBlocks.MOD_ID + "_config").provider(configProvider).request();
-        assignConfigs();
+
+        // assign config
+        BLOCK_SPAWN_CHANCE = MathHelper.clamp(
+                CONFIG.getOrDefault(BLOCK_SPAWN_CHANCE_KEY, BLOCK_SPAWN_CHANCE_DEFAULT), 0.0, 1.0
+        );
     }
 
-    private static void initializeConfig(ModConfigProvider configProvider) {
+    private static void initializeConfig(@NotNull ModConfigProvider configProvider) {
         configProvider.addComment("The chance that an Enderman will be holding a block on spawn");
         configProvider.addComment("Note that this chance is decreased for The End and all modded dimensions");
         configProvider.addComment("Min = 0.0, max = 1.0");
         configProvider.addField(BLOCK_SPAWN_CHANCE_KEY, BLOCK_SPAWN_CHANCE_DEFAULT);
         configProvider.addComment("");
-    }
-
-    private static void assignConfigs() {
-        BLOCK_SPAWN_CHANCE = MathHelper.clamp(
-                CONFIG.getOrDefault(BLOCK_SPAWN_CHANCE_KEY, BLOCK_SPAWN_CHANCE_DEFAULT), 0.0, 1.0
-        );
     }
 }
