5dea1cc871457cd8bac8e0e6397bbff266a9d84e
Nerjal Nosk <nosk.nerjal@gmx.com>
2023-10-16 23:13:47 +0200

    Fix invalid MinecraftServer mixin

--- src/main/java/net/freedinner/endermen_spawn_with_blocks/mixin/MinecraftServerMixin.java
+++ src/main/java/net/freedinner/endermen_spawn_with_blocks/mixin/MinecraftServerMixin.java
@@ -2,20 +2,20 @@ package net.freedinner.endermen_spawn_with_blocks.mixin;
 
 import net.freedinner.endermen_spawn_with_blocks.util.DynamicBlockProvider;
 import net.minecraft.server.MinecraftServer;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
 @Mixin(MinecraftServer.class)
 abstract class MinecraftServerMixin {
 
-    @ModifyVariable(method = "reloadResources", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/CompletableFuture;thenAcceptAsync(Ljava/util/function/Consumer;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;", shift = At.Shift.AFTER))
-    private CompletableFuture<Void> loadDatapackAfterFutureInjection(@NotNull CompletableFuture<Void> completableFuture) {
-        return completableFuture.thenRun(() -> {
-            DynamicBlockProvider.getInstance().process((MinecraftServer)((Object) this));
-        });
+    @Inject(method = "reloadResources", at = @At(value = "INVOKE", shift = At.Shift.AFTER, ordinal = 0, target = "Ljava/util/concurrent/CompletableFuture;supplyAsync(Ljava/util/function/Supplier;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
+    private void loadDatapackAfterFutureSupplyAsyncInjection(Collection<String> dataPacks, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
+        cir.getReturnValue().thenRun(() -> DynamicBlockProvider.getInstance().process((MinecraftServer) ((Object)this)));
     }
 }
